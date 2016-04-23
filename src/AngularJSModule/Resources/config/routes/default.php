fda554077eb2a3082ed87c2635ab921ebb89885c
RamEduard <ramon.calle.88@gmail.com>
2016-04-23 11:11:45 -0430

    Before middleware to accepts JSON Requests on AngularJSModule.

--- src/AngularJSModule/Resources/config/routes/default.php
+++ src/AngularJSModule/Resources/config/routes/default.php
@@ -1,12 +1,29 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 /**
- * Default route
+ * Default routes
  *
  * @author Ramon Serrano <ramon.calle.88@gmail.com>
  */
+
+// Accepts JSON Requests
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+// Angular JS App view
 $app->match("/angularjs", "AngularJSModule\\Controller\\DefaultController::index")
     ->bind("angularjs_index");
 
+// HTTP Basic auth
 $app->match("/api/auth/basic","AngularJSModule\\Controller\\DefaultController::basicAuth")
     ->bind('angularjs_auth_basic');
+
+// Form data auth
+$app->match("/api/auth/form","AngularJSModule\\Controller\\DefaultController::formAuth")
+    ->bind('angularjs_auth_form');
